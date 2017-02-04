3e158883685e81a3c5f81dc0949f884084a1900f
William Ehlhardt <williamehlhardt@gmail.com>
2017-02-04 13:01:32 -0800

    Hack around with chunking

--- cr1-ultracoded/decode.py
+++ cr1-ultracoded/decode.py
@@ -10,12 +10,19 @@ digit = {
     }
 
 data2 = []
-for start in xrange(0, len(data), 8):
-    chunk = data[start:(start+8)]
+
+def chunks(seq, size):
+    for start in xrange(0, len(seq), size):
+        chunk = seq[start:(start+size)]
+        yield chunk
+
+for chunk in chunks(data, 8):
     binary = ''.join(digit[d] for d in chunk)
     num = int(binary, base=2)
     data2.append(chr(num))
 
-print ''.join(data2)
+data2 = ''.join(data2)
 
+for chunk in chunks(data2, 4):
+    print chunk
 
