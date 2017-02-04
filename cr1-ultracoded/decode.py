a6a12387720c32ea6681abad073530d64f2bb26d
William Ehlhardt <williamehlhardt@gmail.com>
2017-02-04 13:45:57 -0800

    Isaak knew what was up.

--- cr1-ultracoded/decode.py
+++ cr1-ultracoded/decode.py
@@ -9,20 +9,16 @@ digit = {
     'ONE' : '1'
     }
 
-data2 = []
 
 def chunks(seq, size):
     for start in xrange(0, len(seq), size):
         chunk = seq[start:(start+size)]
         yield chunk
 
+nums = []
 for chunk in chunks(data, 8):
     binary = ''.join(digit[d] for d in chunk)
     num = int(binary, base=2)
-    data2.append(chr(num))
+    nums.append(num)
 
-data2 = ''.join(data2)
-
-for chunk in chunks(data2, 4):
-    print chunk
-
+print ''.join(map(chr, nums))
